097bf47aaf82e6df9f549ec3e9d1f7fe5baac255
Opeyemi Ojo <54722162+opyjo@users.noreply.github.com>
2025-07-18 12:59:23 -0400

    fix build issues

--- hooks/usePracticeQuiz.ts
+++ hooks/usePracticeQuiz.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
